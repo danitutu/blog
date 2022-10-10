c4b232bc2ca8be2f04a859317acb9b04371105e5
danitutu <valentindanieltutuianu@gmail.com>
2022-10-10 19:12:19 +0300

    add title and about section placeholders

--- src/main/kotlin/com/vdt/blog/ControllerAdvice.kt
+++ src/main/kotlin/com/vdt/blog/ControllerAdvice.kt
@@ -2,6 +2,7 @@ package com.vdt.blog
 
 import com.vdt.blog.tag.TagEntity
 import com.vdt.blog.tag.TagRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ModelAttribute
 
@@ -9,9 +10,21 @@ import org.springframework.web.bind.annotation.ModelAttribute
 @ControllerAdvice
 class ControllerAdvice(
     private val tagRepository: TagRepository,
+    @Value("\${app.title}") private val appTitle: String,
+    @Value("\${app.static-content.section.about}") private val appAboutDescription: String,
 ) {
     @ModelAttribute("tags")
     fun getTags(): Set<TagEntity> {
         return tagRepository.findAll()
+    }
+
+    @ModelAttribute("appTitle")
+    fun getAppTitle(): String {
+        return appTitle
+    }
+
+    @ModelAttribute("appAboutDescription")
+    fun getAppAboutDescription(): String {
+        return appAboutDescription
     }
 }
